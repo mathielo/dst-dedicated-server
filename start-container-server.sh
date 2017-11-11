963de7a9c47a053c8e6c819934482f3f9807582f
Caio Mathielo <mathielo@gmail.com>
2017-11-11 20:14:12 +0100

    Add game update check before spining up the containers

--- start-container-server.sh
+++ start-container-server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Starting DST Dedicated Server: Shard [$SHARD_NAME]"
+# Check for game updates before each start. If the game client updates and your server is out of date, you won't be
+# able to see it on the server list. If that happens just restart the containers and you should get the latest version
+/home/dst/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir /home/dst/server_dst +app_update 343050 validate +quit
 
 cd $HOME/server_dst/bin
 ./dontstarve_dedicated_server_nullrenderer -cluster DSTWhalesCluster -shard "$SHARD_NAME"
