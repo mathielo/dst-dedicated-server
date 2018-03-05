a6c96314b486974d012ab4388419ae3173609aed
Caio Mathielo <mathielo@gmail.com>
2018-03-05 21:44:42 +0100

    Set mods downloads to be persisted and shared among shards

--- start-container-server.sh
+++ start-container-server.sh
@@ -4,19 +4,19 @@
 # able to see it on the server list. If that happens just restart the containers and you should get the latest version
 /home/dst/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir /home/dst/server_dst +app_update 343050 validate +quit
 
-# Copy modoverrides.lua
+# Copy dedicated_server_mods_setup.lua
 ds_mods_setup="$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/mods/dedicated_server_mods_setup.lua"
 if [ -f "$ds_mods_setup" ]
 then
-	cp $ds_mods_setup "$HOME/server_dst/mods/"
+  cp $ds_mods_setup "$HOME/server_dst/mods/"
 fi
 
 # Copy modoverrides.lua
 modoverrides="$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/mods/modoverrides.lua"
 if [ -f "$modoverrides" ]
 then
-	cp $modoverrides "$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/Master/"
-	cp $modoverrides "$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/Caves/"
+  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/Master/"
+  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/Caves/"
 fi
 
 cd $HOME/server_dst/bin
